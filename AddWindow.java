97f5ac58036c358f80910af25b0d394ac77c280d
Thigamore <lascatho000@gmail.com>
2022-01-02 18:46:19 -0600

    removed allergiesDiets and made new one

--- AddWindow.java
+++ AddWindow.java
@@ -1,3 +1,28 @@
-public class AddWindow {
+// Thomas Lascaud
+// 1/2/22
+// Adds data to the database depending on what the user wants to add
+
+import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class AddWindow extends JFrame implements ActionListener{
     
+
+    public AddWindow(String windowName)
+    {
+        super("Add " + windowName);
+
+    }
+
+    //Sets up a general add class 
+    private 
+
+    public static void main()
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
 }
